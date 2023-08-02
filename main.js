458aa30d676d08c89d2c6d53d444bdbfabcec6ca
Ricardo Solari <133733530+cosolvan1@users.noreply.github.com>
2023-08-02 05:26:50 +0000

    avance proyecto

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 "use strict";
+
 function calculoDedescuento(precioServicio, descuentoPorcentaje) {
     let resultado = precioServicio * (1 - descuentoPorcentaje);
     return resultado;
 }
 alert(`
-    ¡Conoce mis servicios!
+    ¿Empezamos ya?¡Conoce mis servicios!
+    
     1. Fotografía Profesional - 1000 soles
     2. Papeleria - 300 soles
     3. Branding - 1000 soles
@@ -12,7 +14,7 @@ alert(`
     5. Lettering- 500 soles
 `)
 
-let servicios = prompt("¿Qué servicio te interesa?");
+let servicios = prompt("¿Qué servicio te interesa? Escribe un número del 1 al 5");
 let precio;
 if (servicios === "1") {
     precio = 1000;
@@ -34,15 +36,15 @@ if (servicios === "1") {
 }
 
 
-let cuestionario = prompt("¿Quieres un descuento en tu primer proyecto?: ");
+let cuestionario = prompt("¿Quieres un descuento del 20% en tu primer proyecto? ¡Solo escribe si!: ");
 if (cuestionario === "si" || cuestionario === "SI" || cuestionario === "Si" || cuestionario === "sI") {
     alert("Gracias, necesitamos la siguiente información")
     let nombre = prompt("escribe tu nombre:      ");
     let apellido = prompt("escribe tu apellido:     ");
     let correo = prompt("escribe tu correo: ");
-    alert(`Estimado cliente ${apellido} se ha guardado sus datos de contacto, nuestra cotización es de ${calculoDedescuento(precio,0.2)}se enviará al correo ${correo}`);
+    alert(`Estimado cliente ${apellido} se generó el descuento, la cotización es de ${calculoDedescuento(precio,0.2)} se enviará al correo ${correo}`);
 } else {
     alert("Te espero nuevamente");
-};
+}
 
 
